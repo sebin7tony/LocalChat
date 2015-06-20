7713b6238f663852faf27965c3b4c1d73f6af824
sebin7tony <sebin7tony@gmail.com>
2015-06-20 16:06:40 +0530

    MySQL Database connection established !!

--- LocalChat/src/main/javas/com/chat/dataimpl/ChatDataImpl.java
+++ LocalChat/src/main/javas/com/chat/dataimpl/ChatDataImpl.java
@@ -1,5 +1,7 @@
 package com.chat.dataimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.chat.data.ChatData;
@@ -8,10 +10,13 @@ import com.chat.models.User;
 @Repository
 public class ChatDataImpl implements ChatData{
 	
-	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	public User getCurrentUser() {
 		
+		String sql = "INSERT INTO tags (tagname) VALUES ('food');";
+		jdbcTemplate.update(sql);
 		User user = new User(1, "sebin", "sebin7tony@gmail.com");
 		return user;
 	}
